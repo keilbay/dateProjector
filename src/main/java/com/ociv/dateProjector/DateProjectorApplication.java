e8fdacb9b91e201fc4c067fb83cfb118ac7b576f
Frank <Frank>
2017-11-13 22:30:28 +0100

    Initial Commit

---
+++ src/main/java/com/ociv/dateProjector/DateProjectorApplication.java
@@ -0,0 +1,12 @@
+package com.ociv.dateProjector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateProjectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateProjectorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ociv/dateProjector/DateProjectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ociv.dateProjector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DateProjectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
